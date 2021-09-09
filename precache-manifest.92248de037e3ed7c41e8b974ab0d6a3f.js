e82a5f3141ea44709a245f5a40f9047cc0b8f351
Aleksey Tsybulko <tsybulko.aleksey@gmail.com>
2021-09-09 22:12:20 +0300

    Updates

---
+++ precache-manifest.92248de037e3ed7c41e8b974ab0d6a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b17b5e6cef429372017c07a284b7af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0350e6378c896a1340f4",
+    "url": "./static/css/main.ab9df5de.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0350e6378c896a1340f4",
+    "url": "./static/js/main.218b7da6.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
